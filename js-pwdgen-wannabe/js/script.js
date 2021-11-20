b344e553b9b1238691f9f0a32c2e5b986a216c5c
OussamaEchi99 <oussama.echi99@gmail.com>
2021-11-20 14:59:46 +0100

    Commentato

--- js-pwdgen-wannabe/js/script.js
+++ js-pwdgen-wannabe/js/script.js
@@ -3,15 +3,17 @@
 // poi chiedi il suo colore preferito
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
-
+// Chiedo il nome all'utente
 const userName = prompt('Dimmi il tuo nome?');
 
-
+// Chiedo il cognome all'utente
 const userLastName = prompt('Dimmi il tuo Cognome?');
 
-
+// Chiedo il suo colore preferito all'utente
 const userColor = prompt('Dimmi il tuo colore preferito?');
 
+// Combino le tre variabili
 let password = `${userName}${userLastName}${userColor}21`;
 
+// Manipolo l'h1 con il id e stampo il messaggio
 document.getElementById('password').innerHTML = password;
