6e63450ab1a541ce2ee92be8be53b3b9e52c618c
OussamaEchi99 <oussama.echi99@gmail.com>
2021-11-21 20:32:29 +0100

    Finito

---
+++ Esercizio di gruppo/js/script.js
@@ -0,0 +1,20 @@
+// // Inserisci un numero se pari lo stampa e dispari non lo stampa
+
+// // Chiedo all'utente un numero
+// let userNumber = parseInt(prompt('Dimmi un numero?'));
+// console.log(userNumber);
+
+// // Se pari lo stampa e dispari non lo stampa
+// if ( userNumber % 2 === 0 ) {
+//     alert('Pari!');
+// }
+
+// ______________________________________________________________
+
+// Stampo con un ciclo For tutti i numeri da 0 a 10
+
+let number = 0;
+for (let i = 0; i <= 10; i++ ) {
+    console.log(number);
+    number++;
+}
